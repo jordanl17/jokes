d2389baad3d8dd550e7cf5455c7e2e96393a0d3f
Jordan Lawrence <jordanl17@me.com>
2019-04-15 19:45:11 +0100

    added final readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Jordan's Healthera Challenge
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+This starts an express server on port 8050; and opens a React app on port 3000
+
+### `cd server > npm test`
+
+Runs a suite of Jest tests - note that fileService.test.js is incomplete due to complexities with mocking the returned file's contents
--- server/test/services/fileService.test.js
+++ server/test/services/fileService.test.js
@@ -24,17 +24,4 @@ describe("fileService", () => {
       expect(readStream.on).toHaveBeenCalledTimes(3);
     });
   });
-
-  describe("getById", () => {
-    it("do thing", () => {
-      fileService.getById(0);
-
-      console.log(fs.createReadStream.mock.results[1].value.on.mock.calls);
-      const readStream = fs.createReadStream.mock.results[1].value;
-
-      const onEnd = readStream.on.mock.calls[0][1];
-      onEnd(['first', 'second']);
-      expect(fs.writeFile).toHaveBeenCalledWith("jokes.csv", "");
-    });
-  });
 });
